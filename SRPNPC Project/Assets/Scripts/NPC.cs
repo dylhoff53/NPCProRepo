d101ccdeb011abd6526d8d81c3b8114b12dacf29
dhoffman548 <112724937+dylhoff53@users.noreply.github.com>
2022-11-14 21:27:34 -0700

    pro

---
+++ SRPNPC Project/Assets/Scripts/NPC.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] private int startingHp = 100;
+    [SerializeField] private ParticleSystem deathParticlePrefab;
+
+    private int currentHp;
+
+    private float CurrentHpPct
+    {
+        get { return (float)currentHp / (float)startingHp; }
+    }
+
+
+    public event Action<float> OnHPPctChanged = delegate { };
+    public event Action OnNPCDied = delegate { };
+
+    private void Start()
+    {
+        currentHp = startingHp;
+    }
+
+    private void UpdateUI()
+    {
+        var currentHpPct = (float)currentHp / (float)startingHp;
+
+        //slider.value = currentHpPct;
+    }
+
+    private void Update()
+    {
+        //hpBarSlider.transform.LookAt(Camera.main.transform);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(startingHp / 10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(20);
+        }
+    }
+
+
+internal void TakeDamage(int amount)
+    {
+        GetComponent<IHealth>().TakeDamage(amount);
+    }
+
+    private void Die()
+    {
+        OnNPCDied();
+        GameObject.Destroy(this.gameObject);
+    }
+}
